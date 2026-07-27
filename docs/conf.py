bdd57c672a667732841d5ed54cf475173b3fd00d
kjshfjhas <kjshfjhas@outlook.com>
2026-07-27 12:46:39 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'safeincloud-account-login'
+author = 'safeincloud-account-login'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
